b3cb39af6cb7e906b9d676f8dac048a45b733ac0
azalea <azaleawang4@gmail.com>
2023-12-03 22:26:35 +0800

    fix/ remove unused react usestate

--- frontend/src/common/hooks/TradingDataContext.tsx
+++ frontend/src/common/hooks/TradingDataContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, ReactNode, useEffect } from "react";
+import React, { createContext, useState, ReactNode } from "react";
 import {
   BotDetailsProps,
   ContainerStateProps,
