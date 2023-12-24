fd97f4dabdfbe5843757d21b03d143692b73cb90
azalea <azaleawang4@gmail.com>
2023-12-24 23:27:00 +0800

    refractor/ update schema and tidyup crud

--- backend/app/crud/backtest.py
+++ backend/app/crud/backtest.py
@@ -3,99 +3,67 @@ from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException
 from app.utils.database import get_db
 from app.models.backtest import BacktestResult
-from app.src.schema import schemas
+from app.src.schema import backtest as schemas
 from sqlalchemy import text, func
+
 
 # If the strategy has been tested before, return the strategy backtest result
 def check_backtest_strategy(
-    strategy, db: Session = Depends(get_db)):
+    strategy: schemas.BacktestStrategy, db: Session = Depends(get_db)
+):
     try:
         backtest_existed = (
             db.query(BacktestResult)
             .filter(
-                BacktestResult.strategy_name == strategy.get('name'),
-                BacktestResult.symbol == strategy.get('symbols')[0],
-                BacktestResult.t_frame == strategy.get('t_frame'),
-                BacktestResult.since == strategy.get('since'),
-                BacktestResult.type == strategy.get('default_type'),
-                BacktestResult.params == strategy.get('params'),
+                BacktestResult.strategy_name == strategy.name,
+                BacktestResult.symbol == strategy.symbols[0],
+                BacktestResult.t_frame == strategy.t_frame,
+                BacktestResult.since == strategy.since,
+                BacktestResult.type == strategy.default_type,
+                BacktestResult.params == strategy.params,
                 BacktestResult.updated_at + text("'1 day'::interval") > func.now(),
             )
             .first()
         )
-        if backtest_existed:
-            print("find backtest: id = ", backtest_existed.id)
-            return backtest_existed.id  # strategy backtest existed
-        else:
-            return None  # strategy not existed
+
+        return backtest_existed.id if backtest_existed else None
+
     except Exception as e:
         logging.error("Error in checking backtest strategy history: %s", e)
         raise HTTPException(
             status_code=500, detail="Check backtest strategy history failed."
         ) from e
-    
-    # return backtest_existed.id  # strategy existed
-    # return None  # strategy not existed
-    
 
-# check if tested before (暫時先不做時間檢查)
+
 def check_backtest_result(
     bt_res: schemas.BacktestResultBase, db: Session = Depends(get_db)
 ):
-    try:
-        backtest_existed = (
-            db.query(BacktestResult)
-            .filter(
-                BacktestResult.strategy_name == bt_res.get("info").get("name"),
-                BacktestResult.symbol == bt_res.get("info").get("symbols")[0],
-                BacktestResult.t_frame == bt_res.get("info").get("t_frame"),
-                BacktestResult.since == bt_res.get("info").get("since"),
-                BacktestResult.type == bt_res.get("info").get("default_type"),
-                BacktestResult.params == bt_res.get("info").get("params"),
-            )
-            .first()
+    backtest_existed = (
+        db.query(BacktestResult)
+        .filter(
+            BacktestResult.strategy_name == bt_res.get("info").get("name"),
+            BacktestResult.symbol == bt_res.get("info").get("symbols")[0],
+            BacktestResult.t_frame == bt_res.get("info").get("t_frame"),
+            BacktestResult.since == bt_res.get("info").get("since"),
+            BacktestResult.type == bt_res.get("info").get("default_type"),
+            BacktestResult.params == bt_res.get("info").get("params"),
         )
-        if backtest_existed:
-            print("find backtest: id = ", backtest_existed.id)
-            return backtest_existed.id  # strategy existed
-        else:
-            return None  # strategy not existed
-    except Exception as e:
-        logging.error("Error in checking backtest result: %s", e)
-        raise HTTPException(
-            status_code=500, detail="Check backtest result failed."
-        ) from e
+        .first()
+    )
+
+    return backtest_existed or None
 
 
-def insert_backtest_result(
-    bt_res: schemas.BacktestResultBase, db: Session = Depends(get_db)
-):
+def insert_backtest_result(bt_res, db: Session = Depends(get_db)):
     try:
-        existed_id = check_backtest_result(bt_res, db)
-        if existed_id:
-            # update plot url and result details
-            updated_result = (
-                db.query(BacktestResult)
-                .filter(BacktestResult.id == existed_id)
-                .update(
-                    {
-                        BacktestResult.plot_url: bt_res.get("result").get("plot"),
-                        BacktestResult.details: bt_res.get("result"),
-                    }
-                )
-            )
-            db.commit()
+        existed_backtest = check_backtest_result(bt_res, db)
+        if existed_backtest:
+            # Update plot url and result details
+            existed_backtest.plot_url = bt_res.get("result").get("plot")
+            existed_backtest.details = bt_res.get("result")
 
-            if not updated_result:
-                raise HTTPException(
-                    status_code=400, detail="Update backtest result failed."
-                )
-            return existed_id
-            # return (
-            #     db.query(BacktestResult)
-            #     .filter(BacktestResult.id == existed_id)
-            #     .first()
-            # )
+            db.commit()
+            return existed_backtest.id
 
         backtest_result = BacktestResult(
             strategy_name=bt_res.get("info").get("name"),
@@ -114,8 +82,8 @@ def insert_backtest_result(
     except Exception as e:
         logging.error("Error in inserting backtest result: %s", e)
         raise HTTPException(
-            status_code=400, detail="Insert backtest result failed."
-        ) from e
+            status_code=500, detail="Insert backtest result failed."
+        )
 
 
 # read backtest result by strategy id
--- backend/app/routers/backtests.py
+++ backend/app/routers/backtests.py
@@ -1,110 +1,75 @@
 import asyncio
-from typing import Any, Union, List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 import os
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Json
 import traceback, logging, json
-from app.crud.backtest import check_backtest_strategy, get_backtest_result, insert_backtest_result
+from app.crud.backtest import (
+    check_backtest_strategy,
+    get_backtest_result,
+    insert_backtest_result,
+)
 from app.utils.database import get_db
 from app.src.controller.sqs import send_sqs_message
-from app.src.schema.schemas import Backtest_Strategy, BacktestResultBase, Message_Resp
+from app.src.schema.commons import MessageResp
+from app.src.schema.backtest import BacktestResultBase, BacktestStrategy
 import websockets
 import math
+from app.constants import BACKTEST_RESULT_EXAMPLE as bt_res
+from app.exceptions import UnexpectedError, BacktestResultNotFound
 
 router = APIRouter()
 
 
-# TODO 送出之前先檢查要不要從資料庫撈出舊資料
-@router.post("/", response_model=Message_Resp)
-def run_backtest(strategy: Backtest_Strategy, db: Session = Depends(get_db)) -> dict:
-    try:
-        print(strategy.model_dump())
-        # check if the strategy backtest result is already in the database
-        check_result = check_backtest_strategy(strategy.model_dump(), db)
-        if check_result:
-            asyncio.run(send_message(strategy.user_id, {"id": check_result}))
-            return {
-                "message": f"Backtesting '{strategy}' already in the database."
-            }    
-        # send message into SQS queue
-        strategy_config = {"s3_url": os.getenv("S3_BACKTEST_STRATEGY_URL")}
-        message_body = dict(**strategy.model_dump(), **strategy_config)
-        response = send_sqs_message(message_body=message_body)
-        # response = {}
-        if "error" in response:
-            print("Error occurred:", response.get("details"))
-            return JSONResponse(
-                content={
-                    "message": f"Backtesting job push into SQS failed. {response.get('details')}"
-                },
-                status_code=500,
-            )
-        else:
-            
-            return {
-                "message": f"Backtesting '{strategy}' job push into SQS id {response.get('MessageId')}."
-            }
+@router.post("/", response_model=MessageResp)
+def run_backtest(strategy: BacktestStrategy, db: Session = Depends(get_db)):
+    # Check if the strategy backtest result is already in the database
+    existed_result_id = check_backtest_strategy(strategy, db)
+    if existed_result_id:
+        asyncio.run(send_message(strategy.user_id, {"id": existed_result_id}))
+        return MessageResp(message=f"Backtesting '{strategy}' already in the database.")
 
-    except HTTPException as http_ex:
-        raise http_ex
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        traceback.print_exc()
-        raise HTTPException(status_code=500, detail=str(e))
+    # Send message into SQS queue
+    strategy_config = {"s3_url": os.getenv("S3_BACKTEST_STRATEGY_URL")}
+    message_body = dict(**strategy.model_dump(), **strategy_config)
+    send_sqs_message(message_body=message_body)
+
+    return MessageResp(
+        message=f"Backtesting '{strategy}' job successfully push into SQS."
+    )
 
 
-bt_res = {
-    "info": {
-        "name": "MaRsi",
-        "symbols": ["BTC/USDT"],
-        "t_frame": "4h",
-        "since": "2023-01-01T00:00:00Z",
-        "default_type": "future",
-        "params": {"rsi_window": 20},
-        "s3_url": "https://my-trading-bot.s3.ap-northeast-1.amazonaws.com/backtest/strategy/",
-    },
-    "result": '{\n    "Start": "2023-01-01 00:00:00",\n    "End": "2023-11-28 08:00:00",\n    "Duration": "331 days 08:00:00",\n    "Exposure Time [%]": 82.08223311957752,\n    "Equity Final [$]": 1355324.8288,\n    "Equity Peak [$]": 1517576.6848,\n    "Return [%]": 35.532482879999996,\n    "Buy & Hold Return [%]": 124.00617171900525,\n    "Return (Ann.) [%]": 39.69092387742143,\n    "Volatility (Ann.) [%]": 57.32197021057216,\n    "Sharpe Ratio": 0.6924207896486616,\n    "Sortino Ratio": 1.6752137367792543,\n    "Calmar Ratio": 1.8197071583421327,\n    "Max. Drawdown [%]": -21.811709480542106,\n    "Avg. Drawdown [%]": -3.649393432304394,\n    "Max. Drawdown Duration": "137 days 13:00:00",\n    "Avg. Drawdown Duration": "8 days 05:00:00",\n    "# Trades": 2,\n    "Win Rate [%]": 50.0,\n    "Best Trade [%]": 36.58727154426143,\n    "Worst Trade [%]": -0.763650456607301,\n    "Max. Trade Duration": "270 days 20:00:00",\n    "Avg. Trade Duration": "135 days 23:00:00",\n    "Profit Factor": NaN,\n    "Expectancy [%]": 17.911810543827063,\n    "SQN": 0.9453312864480757,\n    "_strategy": "MaRsi(params=)",\n    "plot": "backtest/result/MaRsi_BTC-USDT_1h_20231128-082333.html"\n}',
-}
-
-
-@router.post("/result/", response_model=Message_Resp)
+@router.post("/result/", response_model=MessageResp)
 def receive_lambda_result(
     data: BacktestResultBase = bt_res, db: Session = Depends(get_db)
 ):
     try:
-        logging.info(f"Received data from Lambda: {data}")
         parsed_result = data.model_dump()
-        print(parsed_result)
-        # TODO
+
         client_id = int(parsed_result["info"]["user_id"])
         parsed_result["result"] = json.loads(parsed_result["result"])
-        for key, value in  parsed_result["result"].items():
+        for key, value in parsed_result["result"].items():
             if isinstance(value, float) and math.isnan(value):
-                 parsed_result["result"][key] = None
-       
-        # Let's insert data (or redis)
+                parsed_result["result"][key] = None
+
         bt_res_id = insert_backtest_result(parsed_result, db)
-        print("get backtest result id = ", bt_res_id)
-        # notify frontend to fetch new data or refresh page
+
+        # Notify frontend to fetch new data or refresh page
         asyncio.run(send_message(client_id, {"id": bt_res_id}))
-        return {
-            "message": "Data received successfully",
-        }
+        return MessageResp(message="Data received successfully")
     except HTTPException as http_ex:
         raise http_ex
     except Exception as e:
+        traceback.print_exc()
         logging.error(f"Error in receive_lambda_result: {e}")
-        raise HTTPException(
-            status_code=500, detail="Error processing received data." + str(e)
-        )
+        raise UnexpectedError(detail="Error in processing received data from Lambda.")
 
 
 # get backtest result by id
-@router.get("/results/{bt_res_id}")
+@router.get("/results/{bt_res_id}", responses={404: {"description": "Not found"}})
 def get_strategy(bt_res_id: int, db: Session = Depends(get_db)):
     db_backtest_result = get_backtest_result(db, bt_res_id)
+    if not db_backtest_result:
+        raise BacktestResultNotFound()
     return db_backtest_result
 
 
@@ -112,5 +77,5 @@ async def send_message(client_id: int, message={"data": "test"}):
     uri = f"ws://localhost:8000/ws/backtest_result/{client_id}"  # Use localhost when test locally
     async with websockets.connect(uri) as websocket:
         await websocket.send(json.dumps(message))
-        greeting = await websocket.recv()
-        print(f"<<< {greeting}")
+        reply = await websocket.recv()
+        print(f"<<< {reply}")
